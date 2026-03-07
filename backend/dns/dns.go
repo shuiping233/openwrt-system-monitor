42fd868a20111df068d8ad9899393915a1082fa0
shuiping233 <1944680304@qq.com>
2026-03-07 15:39:27 +0800

    feat: 实现ipv6地址dns查询可以通过ipv6邻居反查mac再到ipv4 dns查询,查出hostname

--- backend/dns/dns.go
+++ backend/dns/dns.go
@@ -13,9 +13,10 @@ import (
 const DnsCacheExpireTime = 5 * time.Minute
 
 type DnsQueryService struct {
-	dnsCache     sync.Map
-	resolver     *net.Resolver
-	queryTimeout time.Duration
+	dnsCache        sync.Map
+	resolver        *net.Resolver
+	queryTimeout    time.Duration
+	neighborService *NeighborService
 }
 
 func NewDnsQueryService(dnsIp string, queryTimeout time.Duration) *DnsQueryService {
@@ -28,12 +29,15 @@ func NewDnsQueryService(dnsIp string, queryTimeout time.Duration) *DnsQueryServi
 			return d.DialContext(ctx, "udp", net.JoinHostPort(dnsIp, "53"))
 		},
 	}
+
 	return &DnsQueryService{
-		dnsCache:     sync.Map{},
-		resolver:     resolver,
-		queryTimeout: queryTimeout,
+		dnsCache:        sync.Map{},
+		resolver:        resolver,
+		queryTimeout:    queryTimeout,
+		neighborService: NewNeighborService(),
 	}
 }
+
 func (dqs *DnsQueryService) LookupAddr(ips []string) (model.DnsResult, error) {
 	result := make(model.DnsResult, len(ips))
 
@@ -53,11 +57,16 @@ func (dqs *DnsQueryService) LookupAddr(ips []string) (model.DnsResult, error) {
 		ctx, cancel := context.WithTimeout(context.Background(), dqs.queryTimeout)
 		names, err := dqs.resolver.LookupAddr(ctx, ip)
 		cancel()
-		if err != nil {
+		if err != nil || len(names) == 0 {
 			// 查询失败如果打印出来会导致有几吨的日志
 			// log.Printf("Dns query for %q failed: %s\n", ip, err)
+			names = dqs.LookupIpv6ByNeighborService(ip)
+		}
+
+		if len(names) == 0 {
 			continue
 		}
+
 		hostnameList := make([]string, 0, len(names))
 		for _, name := range names {
 			name = strings.TrimSuffix(name, ".")
@@ -69,6 +78,35 @@ func (dqs *DnsQueryService) LookupAddr(ips []string) (model.DnsResult, error) {
 		})
 		result[ip] = hostnameList
 	}
-
 	return result, nil
+}
+
+func (dqs *DnsQueryService) LookupIpv6ByNeighborService(ip string) []string {
+	result := make([]string, 0, 1)
+	if dqs.neighborService == nil {
+		log.Println("neighbor service is not init , skip Neighbor Service(Mac addr) lookup")
+		return result
+	}
+	// 防止无限递归,也不查ipv4的arp
+	if !strings.Contains(ip, ":") {
+		return result
+	}
+	dqs.neighborService.ActiveSignal()
+
+	// IPv6 -> MAC
+	mac := dqs.neighborService.GetMac(ip)
+	if mac == "" {
+		return result
+	}
+	v4 := dqs.neighborService.GetV4ByMac(mac)
+	if v4 == "" {
+		return result
+	}
+
+	// 拿着 IPv4 去递归查一下主机名
+	v4Res, _ := dqs.LookupAddr([]string{v4})
+	if hostnameList, exists := v4Res[v4]; exists && len(hostnameList) > 0 {
+		result = hostnameList
+	}
+	return result
 }
---
+++ backend/dns/neighbor.go
@@ -0,0 +1,135 @@
+package dns
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"openwrt-diskio-api/backend/model"
+	"sync/atomic"
+	"time"
+
+	"github.com/vishvananda/netlink"
+)
+
+const NeighReloadInterval = 30 * time.Second
+
+type NeighborService struct {
+	ipToMac    atomic.Value
+	macToV4    atomic.Value
+	activeChan chan struct{}
+	lastActive int64
+}
+
+func NewNeighborService() *NeighborService {
+	ns := &NeighborService{
+		activeChan: make(chan struct{}, 1),
+		lastActive: time.Now().UnixNano(),
+	}
+	ns.ipToMac.Store(make(map[string]string))
+	ns.macToV4.Store(make(map[string]string))
+	return ns
+}
+
+func (ns *NeighborService) ActiveSignal() {
+	atomic.StoreInt64(&ns.lastActive, time.Now().UnixNano())
+	select {
+	case ns.activeChan <- struct{}{}:
+	default:
+	}
+}
+
+func (ns *NeighborService) Run(ctx context.Context) {
+	ticker := time.NewTicker(NeighReloadInterval)
+	defer ticker.Stop()
+
+	isRunning := true
+	// TODO 这里可以做netlink.NeighSubscribe,但是处理Neigh事件挺麻烦的
+	// 先不做
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ns.activeChan:
+			if !isRunning {
+				isRunning = true
+				if err := ns.Reload(); err != nil {
+					log.Println(err)
+				}
+			}
+
+		case <-ticker.C:
+			if !isRunning {
+				continue
+			}
+			lastActiveUnix := atomic.LoadInt64(&ns.lastActive)
+			if time.Since(time.Unix(0, lastActiveUnix)) > model.MinServiceRunDuration {
+				isRunning = false
+				continue
+			}
+
+			if err := ns.Reload(); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func (ns *NeighborService) Reload() error {
+	// 构造临时 map
+	nextIpToMac := make(map[string]string)
+	nextMacToV4 := make(map[string]string)
+
+	v4, err := netlink.NeighList(0, netlink.FAMILY_V4)
+	if err != nil {
+		return fmt.Errorf("Get ipv4 neigh list failed: %w", err)
+	}
+	v6, err := netlink.NeighList(0, netlink.FAMILY_V6)
+	if err != nil {
+		return fmt.Errorf("Get ipv6 neigh list failed: %w", err)
+	}
+
+	for _, n := range append(v4, v6...) {
+		if n.State&(netlink.NUD_REACHABLE|netlink.NUD_STALE|netlink.NUD_DELAY) != 0 {
+			ip := n.IP.String()
+			mac := n.HardwareAddr.String()
+			if mac == "" {
+				continue
+			}
+
+			nextIpToMac[ip] = mac
+			if n.IP.To4() != nil {
+				nextMacToV4[mac] = ip
+			}
+		}
+	}
+	ns.ipToMac.Store(nextIpToMac)
+	ns.macToV4.Store(nextMacToV4)
+	return nil
+}
+
+func (ns *NeighborService) GetMac(ip string) string {
+	m, ok := ns.ipToMac.Load().(map[string]string)
+	if !ok {
+		log.Println("Arp search ipToMac map load failed: not a map[string]string type")
+		return ""
+	}
+	result, ok := m[ip]
+	if !ok {
+		return ""
+	}
+	return result
+}
+
+func (ns *NeighborService) GetV4ByMac(mac string) string {
+	m, ok := ns.macToV4.Load().(map[string]string)
+	if !ok {
+		log.Println("Arp search macToV4 map load failed: not a map[string]string type")
+		return ""
+	}
+	result, ok := m[mac]
+	if !ok {
+		return ""
+	}
+	return result
+}
